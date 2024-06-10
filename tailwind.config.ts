8b7f63227191a3795a3cc11c7817f1feb3145076
Sean Sophearom <sean.sophearom77@gmail.com>
2024-06-10 13:25:03 +0700

    feat: Add tailwind.config.ts file with custom theme colors and text underline offset

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.slate,
+        secondary: colors.blue,
+      },
+      textUnderlineOffset: {
+        5: "5px",
+      },
+    },
+  },
+};
