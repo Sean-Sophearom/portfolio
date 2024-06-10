aad025196b6f3fb70e443d216e573110393a88d1
Sean Sophearom <sean.sophearom77@gmail.com>
2024-06-10 13:24:53 +0700

    chore: Update app.vue to include NuxtLoadingIndicator and useHead

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLoadingIndicator />
+  <NuxtPage />
 </template>
+
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  titleTemplate(title) {
+    return title ? `${title} | Sean Sophearom` : "Sean Sophearom";
+  },
+});
+</script>
